dd6043f63e921a4ce88e5ac0ff40e8a8f4ce607f
Ashley8-prog <131078848+Ashley8-prog@users.noreply.github.com>
2023-05-29 17:01:40 +0000

    JS-Project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
 import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = randomnumber();
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let randomnumber = () => {
+  let Pronombre = ["Su", "El"];
+  let quien = ["cobaya", "vecino", "gato", "mamá"];
+  let Accion = ["se comió", "rompió", "rasgó"];
+  let posesion = ["mi tarea", "mi pantalon", "mis zapatos", "mi celular"];
+  let cuando = ["anoche", "Hoy", "en la manaña", "ayer"];
+
+  let pronIndex = Math.floor(Math.random() * Pronombre.length);
+  let quienIndex = Math.floor(Math.random() * quien.length);
+  let AccionIndex = Math.floor(Math.random() * Accion.length);
+  let posesionIndex = Math.floor(Math.random() * posesion.length);
+  let cuandoIndex = Math.floor(Math.random() * cuando.length);
+
+  return (
+    Pronombre[pronIndex] +
+    " " +
+    quien[quienIndex] +
+    " " +
+    Accion[AccionIndex] +
+    " " +
+    posesion[posesionIndex] +
+    " " +
+    cuando[cuandoIndex]
+  );
 };
